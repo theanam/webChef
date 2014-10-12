6bd12799335c6c2387921e8c3dc58feccfb4659c
Nadim Tuhin <nadimtuhin@gmail.com>
2014-10-13 03:25:06 +0600

    reorganized webchef.js and minified it

--- webchef.js
+++ webchef.js
@@ -9,18 +9,20 @@
 
 	var webChef = Object.create(null);
 
-	webChef.cook = function(text,vals){
-		var output = text;
-		var exprs = text.match(/<%\s?\w+\s?%>/ig);
+	webChef.cook = function(text, vals){
+		var output = text,
+			exprs = text.match(/<%\s?\w+\s?%>/ig);
+
 		exprs.forEach(function(el){
 			output = output.replace(el,vals[el.match(/<%\s?(\w+)\s?%>/i)[1]]||"");
 		});
+
 		return output;
 	};
 
 	webChef.makeRecipe=function(text){
 		return function(data){
-			return webChef.cook(text,data);
+			return webChef.cook(text, data);
 		};
 	};
 
