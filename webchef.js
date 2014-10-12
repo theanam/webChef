f9fde31ed3c587aa87c35de841f1f878c1a726cb
Nadim Tuhin <nadimtuhin@gmail.com>
2014-10-13 03:23:42 +0600

    reorganized webchef.js and minified it

--- webchef.js
+++ webchef.js
@@ -1,16 +1,29 @@
-//small templating function
-"use strict";
-var webChef = Object.create(null);
-webChef.cook=function(text,vals){
+/*
+ webChef v1
+ Developed and maintained by [Anam Ahmed](http://anam.co)
+ License: MIT
+*/
+
+(function (window) {
+   'use strict';
+
+	var webChef = Object.create(null);
+
+	webChef.cook = function(text,vals){
 		var output = text;
 		var exprs = text.match(/<%\s?\w+\s?%>/ig);
 		exprs.forEach(function(el){
 			output = output.replace(el,vals[el.match(/<%\s?(\w+)\s?%>/i)[1]]||"");
 		});
 		return output;
-	}
-webChef.makeRecipe=function(text){
-	return function(data){
-		return webChef.cook(text,data);
-	}
-}
+	};
+
+	webChef.makeRecipe=function(text){
+		return function(data){
+			return webChef.cook(text,data);
+		};
+	};
+
+	//expose webChef to global object
+	window.webChef = webChef;
+}(window));
